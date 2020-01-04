9654335a733eb570ac435ca88eca1ef54e9e5b52
Glitch (thespin) <none>
2020-01-04 16:21:18 +0000

    Import of AramZS/thespin

--- public/client.js
+++ public/client.js
@@ -166,7 +166,7 @@ const setColHTML = function(colNum, col) {
 
 window.fillHTMLCols = function(){
   var colNum = 1;
-  var columns = document.querySelectorAll(".collumn .colInner");
+  var columns = document.querySelectorAll(".column .colInner");
   columns.forEach(function(col){
     setColHTML(colNum++, col);
   });
