6e6b3335c32524dd2cf238619886b704564010c3
Glitch (thespin) <none>
2020-07-19 18:16:01 +0000

    💴👹 Checkpoint ./markdown-to-grid.js:1908428/3006

--- markdown-to-grid.js
+++ markdown-to-grid.js
@@ -2,7 +2,7 @@ var showdown = require("showdown");
 var fs = require("fs");
 // var md = require('markdown-it')();
 
-const convert = function(text, toMetadata) {
+const convert = function(text) {
   // showdown.extension('myext', myext);
   var converter = new showdown.Converter({
     strikethrough: true,
@@ -10,7 +10,7 @@ const convert = function(text, toMetadata) {
     tasklists: true,
     metadata: true
   });
-  converter.setFlavor('original');
+  converter.setFlavor("original");
   // var html = md.render(text);
   var html = converter.makeHtml(text);
   var metadata = converter.getMetadata();
@@ -18,5 +18,25 @@ const convert = function(text, toMetadata) {
 };
 
 const getData = function() {
-  
-}
+  const datgridFolder = "./text-datagrid/";
+  return new Promise((resolve, reject) => {
+  fs.readdir(datgridFolder, function(err, filenames) {
+    if (err) {
+      console.log("could not read folder of datagrid", err);
+      resolve(false)
+      return;
+    } else {
+      const data = filenames.map(function(filename) {
+        fs.readFile(datgridFolder + filename, "utf-8", function(err, content) {
+          if (err) {
+            console.log("could not read file of datagrid", err);
+            return;
+          }
+          return (convert(content));
+        });
+      });
+      resolve(data);
+    }
+  });
+  });
+};
