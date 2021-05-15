b0f8c281012f25a6a798a007e14306b198fbc547
Glitch (thespin) <none>
2021-05-15 19:54:17 +0000

    😂🐈 Checkpoint ./clocks.js:1908428/309

--- clocks.js
+++ clocks.js
@@ -21,17 +21,17 @@ const getFactionClocks = (factionName) => {
 // Sets
 const setFactionName = (factionName, abrv) => {
   if(db.get('factions').has({abrv: abrv}).value()){
-    return db.get('factions').find({abrv: abrv}).update('name', factionName).write()
+    return db.get('factions').find({abrv: abrv}).assign({name: factionName}).write()
   } else {
-    return false
+    return { update: false, error: 'Abbreviation not found' }
   }
 }
 
 const setFactionAbrv = (factionName, abrv) => {
   if(db.get('factions').has({name: factionName}).value()){
-    return db.get('factions').find({name: factionName}).update('abrv', abrv).write()
+    return db.get('factions').find({name: factionName}).assign({abrv: abrv}).write()
   } else {
-    return false
+    return {update: false, error: 'Faction name not found'}
   }
 }
 
@@ -39,7 +39,7 @@ const setFactionClocks = (factionAbrv, clockName, level, type) => {
   if(db.get('factions').has({abrv: factionAbrv}).value()){
     
   } else {
-    return false
+    return {update: false, error: 'Faction Abbreviation not found'}
   }
 }
 
