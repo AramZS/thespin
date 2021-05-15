c98ef1c246241dc413117943da910de0561a1c88
Glitch (thespin) <none>
2021-05-15 18:56:10 +0000

    👐☂️ Checkpoint ./clocks.js:1908428/282

--- clocks.js
+++ clocks.js
@@ -3,10 +3,13 @@ const FileSync = require("lowdb/adapters/FileSync");
 const dbapter = new FileSync("factions.json");
 const db = low(dbapter);
 
+
+// All
 const tables = (tableName) => {
   return db.get(tableName).value()
 }
 
+// Gets
 const getFaction = (factionName) => {
   return db.get('factions').find({abrv: factionName}).value()
 }
@@ -14,6 +17,16 @@ const getFaction = (factionName) => {
 const getFactionClocks = (factionName) => {
   return db.get('factions').find({abrv: factionName}).value().clocks
 }
+
+// Sets
+const setFaction = (factionName) => {
+  return db.get('factions').find({abrv: factionName}).value()
+}
+
+const setFactionClocks = (factionName) => {
+  return db.get('factions').find({abrv: factionName}).value().clocks
+}
+
 
 module.exports = {
   db,
