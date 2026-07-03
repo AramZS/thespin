b385325edc5bd7c154b0a901f3361a8182a76ab8
Aram Zucker-Scharff <aramzs@hacktext.com>
2026-07-03 14:26:31 -0400

    Build process update

--- data/site.js
+++ data/site.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
-const baseDomain = process.env.SITE_BASE_DOMAIN || "talesfromthespin.com";
+const baseDomain = process.env.SITE_BASE_DOMAIN || "talesfromthesp.in";
 
 let siteUrl = function (baseDomain) {
 	console.log("this baseDomain", baseDomain);
+	//let port = ''; 
+	let port = process.env.PORT ? ':'+process.env.PORT : '';
 	const protocol = baseDomain.includes("localhost") ? "http" : "https";
-	return `${protocol}://${baseDomain}/`;
+	return `${protocol}://${baseDomain}${port}/`;
 };
 
 const exportableObject = {
